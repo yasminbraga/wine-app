1415213fbe486649ae3b50f5a1f35af4c3bd32f2
yasminbraga <yasminbragateixeira@gmail.com>
2024-02-16 16:25:05 -0300

    add responsive header component

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,7 +1,16 @@
 "use client";
-import { HeaderContainer } from "./styles";
+import {
+  AccountBtn,
+  CartIcon,
+  CountItems,
+  HamburguerBtn,
+  HeaderContainer,
+  IconsContainer,
+  MenuLinks,
+} from "./styles";
 
 import Image from "next/image";
+import { IoIosMenu } from "react-icons/io";
 
 const Logo = () => {
   return <Image src={"/black.png"} alt="wine logo" width={100} height={28} />;
@@ -10,7 +19,34 @@ const Logo = () => {
 const Header: React.FC = () => {
   return (
     <HeaderContainer>
+      <HamburguerBtn>
+        <IoIosMenu size={"2em"} />
+      </HamburguerBtn>
       <Logo />
+      <MenuLinks>
+        <a href="">Clube</a>
+        <a href="">Loja</a>
+        <a href="">Produtores</a>
+        <a href="">Ofertas</a>
+        <a href="">Eventos</a>
+      </MenuLinks>
+
+      <IconsContainer>
+        <a href="">
+          <Image src={"/busca.svg"} alt="search" width={56} height={56} />
+        </a>
+        <AccountBtn href="">
+          <Image src={"/conta.svg"} alt="search" width={56} height={56} />
+        </AccountBtn>
+        <CartIcon>
+          <a href="">
+            <Image src={"/winebox.svg"} alt="search" width={56} height={56} />
+          </a>
+          <CountItems>
+            <p>0</p>
+          </CountItems>
+        </CartIcon>
+      </IconsContainer>
     </HeaderContainer>
   );
 };
--- src/app/components/Header/styles.ts
+++ src/app/components/Header/styles.ts
@@ -2,4 +2,79 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   background-color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 88px;
+  border-bottom: 1px solid #ccc;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.35);
+`;
+
+export const HamburguerBtn = styled.button`
+  background: transparent;
+  border: none;
+
+  @media only screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export const MenuLinks = styled.section`
+  gap: 48px;
+  height: 100%;
+  display: none;
+
+  @media only screen and (min-width: 768px) {
+    display: flex;
+  }
+
+  a {
+    text-decoration: none;
+    color: #555555;
+    font-size: 18px;
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
+
+  a:hover {
+    color: #d14b8f;
+    border-bottom: 2px solid #d14b8f;
+  }
+`;
+
+export const IconsContainer = styled.section`
+  display: flex;
+  gap: 25px;
+
+  @media only screen and (min-width: 768px) {
+    gap: 45px;
+  }
+`;
+
+export const CartIcon = styled.div`
+  position: relative;
+`;
+
+export const AccountBtn = styled.a`
+  display: none;
+
+  @media only screen and (min-width: 768px) {
+    display: inline;
+  }
+`;
+
+export const CountItems = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  background-color: #fff;
+  color: #4fbfa5;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.35);
+  font-weight: 500;
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
 `;
